90f3447649756adc84a74a934c8af5d6094c8620
workingjubilee <workingjubilee@gmail.com>
2019-07-17 01:59:43 -0700

    Found, squashed bug in matrix render

--- SparseMatrix.js
+++ SparseMatrix.js
@@ -35,31 +35,25 @@ class SparseMatrix {
   }
 
   renderMatrix() {
-    const row = new Array(this.dimensions.x).fill(this.defaultValue)
-    const matrix = new Array(this.dimensions.y).fill([...row])
-    let array = []
-    console.log(matrix)
+    const matrix = new Array(this.dimensions.y)
+    for (let i = 0, limit = matrix.length; i < limit; i++) {
+      let row = new Array(this.dimensions.x).fill(this.defaultValue)
+      matrix[i] = row
+    }
 
     for (let [c] of this.matrixMap.entries()) {
       for (let [r] of this.matrixMap.get(c)) {
-        array.push([r,c, this.getElement(c,r)])
+        matrix[r][c] = this.getElement(c,r)
       }
     }
-    console.log(array)
-    let i, limit
-    for (i = 0, limit = array.length; i < limit; i++) {
-      matrix[array[i][0]][array[i][1]] = array[i][2]
-    }
-
-    console.log(matrix[1][2])
 
     return matrix
   }
 }
 
 const matrix = new SparseMatrix(5,5)
-console.log(matrix.setElement(3,4,'blargh'))
-console.log(matrix.setElement(2,1,'nice'))
+console.log(matrix.setElement(3,4,2))
+console.log(matrix.setElement(2,1,1))
 
 // console.log(matrix.getElement(3,4),"test")
 // console.log(matrix.getElement(2,5))
